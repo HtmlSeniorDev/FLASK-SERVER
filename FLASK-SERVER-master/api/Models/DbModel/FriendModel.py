351f7d521534f4071dc25d1331538320caaec09c
BulRUSSIA <man31780@gmail.com>
2020-05-14 21:56:06 +0000

    Добавление Отправки авиков!

---
+++ FLASK-SERVER-master/api/Models/DbModel/FriendModel.py
@@ -0,0 +1,15 @@
+from mongoengine import IntField, StringField, DateTimeField, Document, ReferenceField, CASCADE, FileField
+from api.Models.DbModel.UserModel import User
+
+
+class Friend(Document):
+    creator = ReferenceField(User, reverse_delete_rule=CASCADE)
+    price = IntField()
+    createdAt = DateTimeField()
+    photo = FileField()
+    name = StringField(max_length=16)
+    description = StringField(max_length=30)
+    meta = {'collection': 'gifts'}
+
+    def __str__(self) -> str:
+        return 'New Gifts: ' + str(self.name)
